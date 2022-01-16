f51f87d98936ac6b6e312476674b38804f626d87
Nikitin <dennilitin60908@gmail.com>
2022-01-16 22:28:09 +0300

    5 commit

--- DjangoRestApisPostgreSQL/settings.py
+++ DjangoRestApisPostgreSQL/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 import dj_database_url
 
@@ -139,3 +139,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+django_heroku.settings(locals())
