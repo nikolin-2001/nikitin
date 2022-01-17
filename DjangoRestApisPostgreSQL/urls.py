8f07d389f3a7133323f78d640a14c4e8b5b2a8e9
Nikitin <dennilitin60908@gmail.com>
2022-01-17 11:36:54 +0300

    16 commit

--- DjangoRestApisPostgreSQL/urls.py
+++ DjangoRestApisPostgreSQL/urls.py
@@ -2,6 +2,6 @@ from django.urls import re_path, path, include
 from django.contrib import admin
 
 urlpatterns = [
-    re_path(r'^', include('tutorials.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'api/', include('tutorials.urls')),
 ]
