7267765419891143d97803b3ffb12cc9f688b4b0
Nikitin <dennilitin60908@gmail.com>
2022-01-17 11:31:09 +0300

    15 commit

--- tutorials/urls.py
+++ tutorials/urls.py
@@ -3,9 +3,9 @@ from .views import komplekt_list, komplekt_detail, komplekt_list_published, musi
 
 urlpatterns = [
     re_path(r'^api/tutorials$', komplekt_list),
-    re_path(r'^api/tutoria  ls/(?P<pk>[0-9]+)$', komplekt_detail),
+    re_path(r'^api/tutorials/(?P<pk>[0-9]+)$', komplekt_detail),
     re_path(r'^api/tutorials/published$', komplekt_list_published),
-    re_path(r'^api/music$', music_list),
-    re_path(r'^api/music/(?P<pk>[0-9]+)$', music_detail),
-    re_path(r'^api/music/published$', music_list_published),
+    re_path(r'^api/musics$', music_list),
+    re_path(r'^api/musics/(?P<pk>[0-9]+)$', music_detail),
+    re_path(r'^api/musics/published$', music_list_published),
 ]
